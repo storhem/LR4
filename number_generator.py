f3cf4cc15d3a751546b38d26fc0a4cd2c6b1ec2a
storhem <storhetmax@mail.ru>
2025-11-25 14:10:22 +0300

    added output and test

--- number_generator.py
+++ number_generator.py
@@ -4,3 +4,7 @@ import itertools
 def infinity_random():
     for _ in itertools.count():
         yield random.randint(-9999, 9999)
+
+inp = int(input("Введите количество чисел: "))
+
+print(list(itertools.islice(infinity_random(), inp)))
